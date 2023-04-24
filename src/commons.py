c5d71e118730c0110521c19ba0671f7d2aff5ab4
Kacper Kafara <kacperkafara@gmail.com>
2023-04-24 12:35:30 +0200

    Move API_KEY resolution to commons module

--- src/commons.py
+++ src/commons.py
@@ -1,3 +1,7 @@
+import os
+
 CAMERA_1_SID = "Q2LV-KGF5-GDKS"
 CAMERA_2_SID = "Q2LV-5CU2-FJ6R"
 
+API_KEY = os.getenv('MERAKI_DASHBOARD_API_KEY')
+assert API_KEY is not None, "API Key must not be None"
