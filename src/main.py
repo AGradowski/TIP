28811e9b032c630b04ef6fbba03b739b4fbdfded
Kacper Kafara <kacperkafara@gmail.com>
2023-04-24 12:45:20 +0200

    Add simple API usecase (#3)
    * Add cameras serial numbers in `commons` module
    
    * Move API_KEY resolution to commons module
    
    * Basic request in main module

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import meraki
+import os
+from commons import API_KEY, CAMERA_1_SID
 
-print("hello world")
+meraki_client = meraki.DashboardAPI(API_KEY)
+response = meraki_client.camera.getDeviceCameraCustomAnalytics(CAMERA_1_SID)
+
+print(response)
 
