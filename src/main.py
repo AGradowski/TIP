60e56657d1be6e0d661f6276146311ec803fd265
Antoni Gradowski <antoni.gradowski@gmail.com>
2023-04-24 11:50:42 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,19 @@
 import meraki
+import os
 
-print("hello world")
 
+# Defining your API key as a variable in source code is not recommended
+API_KEY =os.getenv('CAMERA_API_KEY')
+#print(API_KEY)
+# Instead, use an environment variable as shown under the Usage section
+# @ https://github.com/meraki/dashboard-api-python/
+
+dashboard = meraki.DashboardAPI(API_KEY)
+
+serial = 'Q2LV-KGF5-GDKS'
+
+response = dashboard.camera.getDeviceCameraCustomAnalytics(
+    serial
+)
+
+print(response)
